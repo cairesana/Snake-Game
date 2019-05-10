b76c5b88a58cdb80046e421d551854eb4fe290d5
cairesana <caires.ana@gmail.com>
2019-05-10 10:49:04 +0200

    some refactoring before continue

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var game = {};
 
 game.canvas = document.getElementById("myCanvas");
-game.frameRate = 100; // velocidade do jogo - tempo que atualizo a tela milisegundos
+game.frameRate = 90; // game veloc. - update screen (miliseconds)
 game.snake = {
                 direction: 'south',
-                bodyPartSize: 8, //12
+                bodyPartSize: 12, 
                 body: [
                         {x:400, y:80},     // create two balls = snake's head
                         {x:400, y:68}
@@ -12,12 +12,21 @@ game.snake = {
         };
 
 game.apples = [];
-game.gameOver = 0; // 0 = false
+game.gameOver = 0; 
 
 var score = 0;
+var playersName = window.prompt("Player's name -- maximum of 12 characters: ");
+while (playersName.length > 12) {
+        playersName = window.prompt("Please, try again with a maximum of 12 characters: ");
+        continue;
+}
+
 
 // create boardgame using canvas
-var ctx = game.canvas.getContext("2d"); // getContext() is a built-in html object, with properties and methods for drawing. ctx var will store the 2D rendering context (the actual tool we can use to paint on the Canvas).
+// getContext(): built-in html obj with properties and methods for drawing. 
+// ctx var will store the 2D rendering context (the actual tool we can use to paint on the Canvas).
+var ctx = game.canvas.getContext("2d"); 
+
 
 // borders
 var canvasBorder = game.canvas.getContext("2d");
@@ -26,44 +35,39 @@ function drawBorders() {
         canvasBorder.beginPath();
         canvasBorder.lineWidth=10;
         canvasBorder.strokeStyle="red";
-        canvasBorder.rect(5, 40, 790, 555); //(x,y,width,height);
+        canvasBorder.rect(5, 40, 790, 555); //(x, y, width, height);
         canvasBorder.stroke();
         canvasBorder.closePath();
 };
 
 
-// draw apple in canvas (snake's food colored ball): 
-// create apple random location
+// create green apple 
 function createNewApple() { 
         //maxRandomX = 780
         //minRandomX = 10
         //maxRandomY = 585
         //minRandomY = 50
+        // TO DO: test this option: var randomX = Math.floor(Math.random() * (max - min + 1)) + min; pra range entre numeros
         var randomX = Math.floor(Math.random() * Math.floor(800)); // (myCanvas 800 x 600) 10 e 780
         var randomY = Math.floor(Math.random() * Math.floor(600)); // 50 e 585
         
         if (randomX >= 10 && randomX <= 780 && randomY >= 50 && randomY <= 585) {
-                game.apples.push({x: randomX, y: randomY});        
+                game.apples.push({x: randomX, y: randomY});  //saves apple in apple's []      
         } else {
-                console.log("out of range")
                 createNewApple() 
         }
-
-        //game.apples.push({x: randomX, y: randomY}); //saves apple in apple's []
-        console.log("x: ", randomX)
-        console.log("y: ", randomY)
-        // return Math.floor(Math.random() * (max - min + 1)) + min; pra range entre numeros
 }
 
+// draw green apple
 var canvasApple = game.canvas.getContext("2d");
 function drawApple() {
         game.apples.forEach(function(element) {
-                canvasApple.beginPath();    // starts with beginPath and ends with closePath methods.. 
-                canvasApple.arc(element.x, element.y, 3, 0, Math.PI*2, true); // (x, y (coordinates of arc's center in radious), start angle, end angle (to start and finish drawing in radians), direction of drawing, false: clockwise, true: anti-clockwise)
-                canvasApple.fillStyle = "rgba(0, 255, 0, 1)"; // fillStyle property stores a color that will be used by the fill() method 
-                canvasApple.fill(); // paints the circle
-                canvasApple.strokeStyle = "rgba(0, 0, 0, 0)"; // rgba - red green blue alpha (a= opacity) - border's color
-                canvasApple.stroke(); // add border
+                canvasApple.beginPath();     
+                canvasApple.arc(element.x, element.y, 3, 0, Math.PI*2, true); // x, y: coordinates of arc's center (radious); start angle; end angle (start and finish drawing in radians); PI: ratio of the circumference of a circle; true: anti-clockwise (drawing's direction) 
+                canvasApple.fillStyle = "rgba(0, 255, 0, 1)"; // fillStyle property stores a color  
+                canvasApple.fill(); // paints the circle using the color stored in fillstyle
+                canvasApple.strokeStyle = "rgba(0, 0, 0, 0)"; 
+                canvasApple.stroke(); 
                 canvasApple.lineWidth=0;
                 canvasApple.closePath();
         });
@@ -88,19 +92,19 @@ function moveSnake() {
         var newY;
 
         switch (game.snake.direction) {
-                case 'north': // cima -- decrementa o y e permanece x igual
-                        newX = game.snake.body[0].x;  // [0] = getting sneak's head (first ball)
+                case 'north': // cima: decrementa y; permanece x igual
+                        newX = game.snake.body[0].x;  
                         newY = game.snake.body[0].y - game.snake.bodyPartSize;
                         break;
-                case 'east': // direita -- incrementa o x e permanece y igual
+                case 'east': // direita: incrementa x; permanece y igual
                         newX = game.snake.body[0].x + game.snake.bodyPartSize;
                         newY = game.snake.body[0].y;
                         break;
-                case 'south': // baixo -- incrementa o y e permanece x igual
+                case 'south': // baixo: incrementa y; permanece x igual
                         newX = game.snake.body[0].x;
                         newY = game.snake.body[0].y + game.snake.bodyPartSize;
                         break;
-                case 'west': // esquerda -- decrementa o x e permanece y igual
+                case 'west': // esquerda: decrementa o x; permanece y igual
                         newX = game.snake.body[0].x - game.snake.bodyPartSize;
                         newY = game.snake.body[0].y;
                         break;
@@ -108,42 +112,43 @@ function moveSnake() {
 
                 game.snake.body.pop();
                 game.snake.body.unshift({x: newX, y: newY});
-                // game.snake.body.reverse();
                 //console.log(game.snake.body);
         
 }
 
 
 // collision detector 
+// TO DO: collision also for: snake hits its own body  
 function collisionDetector() {
-        // Detect colisao com a maca
+        // collision with apple
         game.apples.forEach(function(element, index, object) {
                 if (
-                    (element.x >= game.snake.body[0].x && (element.x + 3.0) <= (game.snake.body[0].x + game.snake.bodyPartSize)) //1.5
-                    && (element.y >= game.snake.body[0].y && (element.y + 3.0) <= (game.snake.body[0].y + game.snake.bodyPartSize))
+                    (element.x >= game.snake.body[0].x && (element.x + 1.5) <= (game.snake.body[0].x + game.snake.bodyPartSize)) 
+                    && (element.y >= game.snake.body[0].y && (element.y + 1.5) <= (game.snake.body[0].y + game.snake.bodyPartSize))
                 ) {
                         removeApple(index, object);
                         growSnake();
                         score++;
                 }
-        });
+        }); // to do: improve it - sometimes it doesn't "eats" the apple 
+                
 
-        // detect collision with borders -- x.left(20) - x.right(780) - y.top(55) - y.bottom(580)
+        // collision against borders - x.left(20); x.right(780); y.top(55); y.bottom(580)
         if (game.snake.body[0].x < 20 || game.snake.body[0].x > 780 || game.snake.body[0].y < 55 || game.snake.body[0].y > 580) { 
                 console.log("GAME OVER");
                 drawGameOverMessage();
                 drawPlayAgainMessage();
-                game.gameOver = 1; // 1 = true
+                game.gameOver = 1; 
         }
 }
 
-// apple gets removed when collision is detected  
+// remove apple when collision is detected  
 function removeApple(index, object) {
         object.splice(index, 1);
-        console.log(object, index);  //remover depois
+        // console.log(object, index);  
 }
 
-//after eating apple snakes get bigger   
+// grow snake     
 function growSnake() {
         var snakeLastBodyPart = game.snake.body[game.snake.body.length - 1];
         var newBodyPartX;
@@ -154,7 +159,7 @@ function growSnake() {
         //console.log("grow snake", game.snake.body);
 } 
 
-// draws a "game over" message (collision with border for now; for the future: also on snakes own body) 
+// draw "game over" message (when collision against borders)
 var gameOverMessage = game.canvas.getContext('2d');
 
 function drawGameOverMessage() {
@@ -179,7 +184,7 @@ function drawPlayAgainMessage() {
         gradientPlayAgain.addColorStop("0.2","indigo");
         gradientPlayAgain.addColorStop("0","violet");
 
-        // fill with gradient
+        // fill in with gradient
         playAgainMsg.fillStyle=gradientPlayAgain;
         playAgainMsg.fillText("Press ENTER to play again! :)",170,460);        
 }
@@ -193,12 +198,21 @@ function drawScore() {
         drawScoreOnCanvas.fillText("Score: "+score, 20, 20);
 }
 
+// players name
+var drawPlayersNameOnCanvas = game.canvas.getContext('2d');
+console.log('player: ', playersName) 
+
+function drawPlayersName() {
+        x = 20
+        drawPlayersNameOnCanvas.font = "bold 18px Arial";
+        drawPlayersNameOnCanvas.fillStyle = "Gray";
+        drawPlayersNameOnCanvas.fillText("Player: "+playersName, 600, 20); 
+        // TO DO: melhorar a localizao de acordo com tamanho de letras
+}
+
 // restart when game over
 function restartGame() {
         location.reload();  // reload page
-        //ctx.clearRect(0, 0, 800, 600); //nao funfou
-        // game.gameOver = 0; 
-        // execGameLoop();
         }      
       
 // setInterval(drawSnake, 10); //execute draw function every 10 miliseconds - instead setinterval, I could also apply requestAnimationFrame().
@@ -206,6 +220,7 @@ function execGameLoop() {
         ctx.clearRect(0, 0, 800, 600);
         drawBorders();
         drawScore();
+        drawPlayersName(); 
         collisionDetector();
         if (game.gameOver === 0) {
                 moveSnake();
@@ -228,14 +243,14 @@ document.addEventListener('keydown', function(event) {
 
         switch (game.snake.direction) {
                 case 'north':
-                case 'south': //quando indo p/baixo so pode virar pra esq ou dir
+                case 'south': // going down: somente possibilidade de virar para esq ou dir
                         switch (keyCode) {
-                                case 37: //tecla left
+                                case 37: // key: left
                                         game.snake.direction = 'west';
-                                        event.preventDefault(); //impede de rolar tela com tecla seta
+                                        event.preventDefault(); // previne uso da tecla setas de rolar a tela  
                                         break;
                 
-                                case 39: //tecla right
+                                case 39: // key: right
                                         game.snake.direction = 'east';
                                         event.preventDefault();
                                         break;
@@ -250,12 +265,12 @@ document.addEventListener('keydown', function(event) {
                 case 'west':
                 case 'east': //quando indo p/baixo so pode virar pra esq ou dir
                         switch (keyCode) {            
-                                case 38: //tecla up
+                                case 38: // key: up
                                         game.snake.direction = 'north';
                                         event.preventDefault();
                                         break; 
                      
-                                case 40: // tecla down
+                                case 40: // key: down
                                         game.snake.direction = 'south';
                                         event.preventDefault();
                                         break;
@@ -269,7 +284,7 @@ document.addEventListener('keydown', function(event) {
 });
 
 document.addEventListener('keypress', function (e) {
-        var keyEnter = e.keyCode;  //ou seria keyEnter? nome da var
+        var keyEnter = e.keyCode;  
         console.log(e);
 
         if (keyEnter === 13) {
