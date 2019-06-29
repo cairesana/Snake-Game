30155be44a4a1e4fe75c2f61cbc2a5ea33288d8f
cairesana <caires.ana@gmail.com>
2019-06-29 20:24:45 +0200

    added player's name as dynamic text and refactored code a bit.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var game = {};
+var game = {}; // objeto
 
-game.canvas = document.getElementById("myCanvas");
+game.canvas = document.getElementById('myCanvas');
 game.frameRate = 90; // game veloc. - update screen (miliseconds)
 game.snake = {
                 direction: 'south',
@@ -14,10 +14,14 @@ game.snake = {
 game.apples = [];
 game.gameOver = 0; 
 
-var score = 0;
-var playersName = window.prompt("Player's name -- maximum of 12 characters: ");
-while (playersName.length > 12) {
-        playersName = window.prompt("Please, try again with a maximum of 12 characters: ");
+game.score = 0;
+game.playerName = '';
+game.playerName = window.prompt('Player\'s name -- maximum of 12 characters: ');
+if (game.playerName == ''){
+        game.playerName = 'anonymous';
+}
+while (game.playerName.length > 12) {
+        game.playerName = window.prompt('Please, try again with a maximum of 12 characters: ');
         continue;
 }
 
@@ -25,16 +29,16 @@ while (playersName.length > 12) {
 // create boardgame using canvas
 // getContext(): built-in html obj with properties and methods for drawing. 
 // ctx var will store the 2D rendering context (the actual tool we can use to paint on the Canvas).
-var ctx = game.canvas.getContext("2d"); 
+var ctx = game.canvas.getContext('2d'); 
 
 
 // borders
-var canvasBorder = game.canvas.getContext("2d");
+var canvasBorder = game.canvas.getContext('2d');
 
 function drawBorders() {
         canvasBorder.beginPath();
         canvasBorder.lineWidth=10;
-        canvasBorder.strokeStyle="red";
+        canvasBorder.strokeStyle='red';
         canvasBorder.rect(5, 40, 790, 555); //(x, y, width, height);
         canvasBorder.stroke();
         canvasBorder.closePath();
@@ -59,14 +63,14 @@ function createNewApple() {
 }
 
 // draw green apple
-var canvasApple = game.canvas.getContext("2d");
+var canvasApple = game.canvas.getContext('2d');
 function drawApple() {
         game.apples.forEach(function(element) {
                 canvasApple.beginPath();     
                 canvasApple.arc(element.x, element.y, 3, 0, Math.PI*2, true); // x, y: coordinates of arc's center (radious); start angle; end angle (start and finish drawing in radians); PI: ratio of the circumference of a circle; true: anti-clockwise (drawing's direction) 
-                canvasApple.fillStyle = "rgba(0, 255, 0, 1)"; // fillStyle property stores a color  
+                canvasApple.fillStyle = 'rgba(0, 255, 0, 1)'; // fillStyle property stores a color  
                 canvasApple.fill(); // paints the circle using the color stored in fillstyle
-                canvasApple.strokeStyle = "rgba(0, 0, 0, 0)"; 
+                canvasApple.strokeStyle = 'rgba(0, 0, 0, 0)'; 
                 canvasApple.stroke(); 
                 canvasApple.lineWidth=0;
                 canvasApple.closePath();
@@ -75,7 +79,7 @@ function drawApple() {
         
 
 // draw snake 
-var initialSnake = game.canvas.getContext("2d");
+var initialSnake = game.canvas.getContext('2d');
 
 function drawSnake() {
                 game.snake.body.forEach(function (element) {
@@ -128,14 +132,14 @@ function collisionDetector() {
                 ) {
                         removeApple(index, object);
                         growSnake();
-                        score++;
+                        game.score++;
                 }
-        }); // to do: improve it - sometimes it doesn't "eats" the apple 
+        }); // to do: improve it - sometimes it doesn't 'eats' the apple 
                 
 
         // collision against borders - x.left(20); x.right(780); y.top(55); y.bottom(580)
         if (game.snake.body[0].x < 20 || game.snake.body[0].x > 780 || game.snake.body[0].y < 55 || game.snake.body[0].y > 580) { 
-                console.log("GAME OVER");
+                console.log('GAME OVER');
                 drawGameOverMessage();
                 drawPlayAgainMessage();
                 game.gameOver = 1; 
@@ -156,10 +160,10 @@ function growSnake() {
         //console.log(snakeLastBodyPart);
              
         game.snake.body.splice(game.snake.body.length, 0, {x: newBodyPartX, y: newBodyPartY});
-        //console.log("grow snake", game.snake.body);
+        //console.log('grow snake', game.snake.body);
 } 
 
-// draw "game over" message (when collision against borders)
+// draw 'game over' message (when collision against borders)
 var gameOverMessage = game.canvas.getContext('2d');
 
 function drawGameOverMessage() {
@@ -177,50 +181,48 @@ var gradientPlayAgain = playAgainMsg.createLinearGradient(0,0,game.canvas.width,
 function drawPlayAgainMessage() {
         playAgainMsg.font = '40px San serif';
         //create gradient
-        gradientPlayAgain.addColorStop("0.82","red");
-        gradientPlayAgain.addColorStop("0.7","yellow");
-        gradientPlayAgain.addColorStop("0.6","green");
-        gradientPlayAgain.addColorStop("0.4","blue");
-        gradientPlayAgain.addColorStop("0.2","indigo");
-        gradientPlayAgain.addColorStop("0","violet");
+        gradientPlayAgain.addColorStop('0.82','red');
+        gradientPlayAgain.addColorStop('0.7','yellow');
+        gradientPlayAgain.addColorStop('0.6','green');
+        gradientPlayAgain.addColorStop('0.4','blue');
+        gradientPlayAgain.addColorStop('0.2','indigo');
+        gradientPlayAgain.addColorStop('0','violet');
 
         // fill in with gradient
         playAgainMsg.fillStyle=gradientPlayAgain;
-        playAgainMsg.fillText("Press ENTER to play again! :)",170,460);        
+        playAgainMsg.fillText('Press ENTER to play again! :)', 170, 460);        
 }
 
 // score
 var drawScoreOnCanvas = game.canvas.getContext('2d'); 
 
 function drawScore() {
-        drawScoreOnCanvas.font = "bold 18px Arial";
-        drawScoreOnCanvas.fillStyle = "white";
-        drawScoreOnCanvas.fillText("Score: "+score, 20, 20);
+        drawScoreOnCanvas.font = 'bold 18px Arial';
+        drawScoreOnCanvas.fillStyle = 'white';
+        drawScoreOnCanvas.fillText('Score: ' + game.score, 20, 20);
 }
 
 // players name
-var drawPlayersNameOnCanvas = game.canvas.getContext('2d');
-console.log('player: ', playersName) 
+var drawPlayerNameOnCanvas = game.canvas.getContext('2d');
 
-function drawPlayersName() {
-        x = 20
-        drawPlayersNameOnCanvas.font = "bold 18px Arial";
-        drawPlayersNameOnCanvas.fillStyle = "Gray";
-        drawPlayersNameOnCanvas.fillText("Player: "+playersName, 600, 20); 
-        // TO DO: melhorar a localizao de acordo com tamanho de letras
-}
+function drawPlayerName() {
+        var fixedPlayerText = 'Player: ';
+        var textSize = (game.playerName.length + fixedPlayerText.length) * 10;  
+        drawPlayerNameOnCanvas.font = 'bold 18px Mono'; // monospace - all letters have equal sizes 
+        drawPlayerNameOnCanvas.fillStyle = 'Gray';
+        drawPlayerNameOnCanvas.fillText(fixedPlayerText + game.playerName, 780-textSize, 20); 
+}       
 
 // restart when game over
 function restartGame() {
         location.reload();  // reload page
         }      
       
-// setInterval(drawSnake, 10); //execute draw function every 10 miliseconds - instead setinterval, I could also apply requestAnimationFrame().
 function execGameLoop() {
         ctx.clearRect(0, 0, 800, 600);
         drawBorders();
         drawScore();
-        drawPlayersName(); 
+        drawPlayerName(); 
         collisionDetector();
         if (game.gameOver === 0) {
                 moveSnake();
